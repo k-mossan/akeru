abff04831dadae434b33600e0e7d7b299d53a283
k-mori <k-mori@k-morinoMBP.kcn.ne.jp>
2020-12-26 18:50:18 +0900

    チュートリアルの準備

---
+++ Assets/Scripts/Game/Tutorial/TutorialData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TutorialData/Create Data")]
+public class TutorialData : ScriptableObject
+{
+    [System.Serializable]
+    public struct Data
+    {
+        public int m_stage;
+        public int m_notes;
+        public Sprite m_sprite;
+    }
+
+    [SerializeField]
+    private Data[] m_dataList = null;
+
+    public Sprite GetSprite(int stage, int notes)
+    {
+        if (m_dataList.Any(data => data.m_stage == stage && data.m_notes == notes))
+        {
+            return m_dataList.First(d => d.m_stage == stage && d.m_notes == notes).m_sprite;
+        }
+        return null;
+    }
+}
